d743091f27b2205c462242a6808a44960e6c5cb9
Sébastien <sebastien.mariaux@protonmail.com>
2023-03-25 14:29:42 +0100

    Translations

--- reverse_midjourney/settings.py
+++ reverse_midjourney/settings.py
@@ -149,3 +149,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, "locale"),
+]
+
+LANGUAGE_CODE = 'fr'
+
+LANGUAGES = [
+    ('en','English'),
+    ('fr', 'French')
+]
